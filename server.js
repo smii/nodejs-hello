808b3104f4faa6eb6452b09f68dccb0c1b05b86e
Miguel Lopes <miguel.lopes@devoteam.com>
2019-04-03 11:08:52 +0200

    change welcome message

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Welcome to OpenShift_1!! ' + os.hostname() + '\n');
+  res.send('Welcome SVB!! ' + os.hostname() + '\n');
 });
 
 app.listen(PORT,'0.0.0.0');
